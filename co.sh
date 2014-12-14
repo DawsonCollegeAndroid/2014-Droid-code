172f8ae67c31d9fac46d0f6553db8775b1597fd8
P Campbell <triciamontreal@gmail.com>
2014-12-13 21:26:24 -0500

    Update co.sh

--- co.sh
+++ co.sh
@@ -1,3 +1,3 @@
 for i in $(cat list.txt) ; do
-svn --username pcampbell --password yellow7snail co https://waldo2.dawsoncollege.qc.ca/svn/Samples518/$i
+svn --username XXXXXX --password XXXXXX co https://waldo2.dawsoncollege.qc.ca/svn/Samples518/$i
 done 
